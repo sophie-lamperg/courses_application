3fe60aaacfc4dc2135cfb959606e42f48867cb73
OUT-Stetsura-SV <svstetsura@sberbank.ru>
2021-03-10 04:06:21 +0300

    authorize

--- node-express/index.js
+++ node-express/index.js
@@ -1,30 +1,72 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
+const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
+const MONGODB_URI = `mongodb+srv://sofi:koZBc9meB1wlJBao@cluster0.1p1aw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const app = express()
-
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  store
+}))
+app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      // useUnifiedTopology: true
+    })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
---
+++ node-express/routes/auth.js
@@ -0,0 +1,67 @@
+const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+const router = Router()
+
+router.get('/login', async (req, res) => {
+  res.render('auth/login', {
+    title: 'Авторизация',
+    isLogin: true
+  })
+})
+
+router.get('/logout', async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/auth/login#login')
+  })
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body
+    const candidate = await User.findOne({ email })
+
+    if (candidate) {
+      const areSame = await bcrypt.compare(password, candidate.password)
+
+      if (areSame) {
+        req.session.user = candidate
+        req.session.isAuthenticated = true
+        req.session.save(err => {
+          if (err) {
+            throw err
+          }
+          res.redirect('/')
+        })
+      } else {
+        res.redirect('/auth/login#login')
+      }
+    } else {
+      res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const {email, password, repeat, name} = req.body
+    const candidate = await User.findOne({ email })
+
+    if (candidate) {
+      res.redirect('/auth/login#register')
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10)
+      const user = new User({
+        email, name, password: hashPassword, cart: {items: []}
+      })
+      await user.save()
+      res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+module.exports = router
